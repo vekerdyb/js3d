f2a5c0b17053767a2aeb05877361f7a4a57ff286
Balint Vekerdy <vekerdyb@gmail.com>
2015-04-06 17:45:24 +0200

    circles can bed added to the svg canvas

--- app/svg-canvas/svg-canvas.js
+++ app/svg-canvas/svg-canvas.js
@@ -6,6 +6,51 @@ export default ngModule => {
 
   class SvgCanvasManager {
 
+    init(elem) {
+      this.objects = [];
+      this.elem = elem;
+
+      this.htmls = {
+        "circle": "<circle></circle>"
+      };
+    }
+
+    addHtml(tag, attrsObj) {
+      let html = tag;
+      let attrs = "";
+      Object.keys(attrsObj).forEach(attr => {
+        var value = attrsObj[attr];
+        if (angular.isDefined(value) && value !== null) {
+          attrs += ` ${attr}=${value}`
+        }
+      });
+      this.elem.append(`<${html} ${attrs}></${html}>`);
+    }
+
+    addObject(object) {
+      if (object) {
+        this.objects.push(object);
+        Object.keys(object).forEach(key => {
+          this.addHtml(key, object[key]);
+        });
+      } else {
+        console.error("SVG-Canvas: Object not defined")
+      }
+    }
+
+    circle(x=0, y=0, r=1, stroke="black", strokeWidth=1, fill="white") {
+      var circle = {
+        "circle" : {
+          cx: x,
+          cy: y,
+          r: r,
+          stroke: stroke,
+          "stroke-width": strokeWidth,
+          fill: fill
+        }
+      };
+      this.addObject(circle);
+    }
   }
 
   ngModule.service('SvgCanvasManager', SvgCanvasManager);
--- app/svg-canvas/svg-canvas.test.js
+++ app/svg-canvas/svg-canvas.test.js
@@ -15,5 +15,66 @@ export default ngModule => {
     it(`should exist`, () => {
       expect(sut).toBeDefined();
     });
-  })
+
+    describe(`initializer method`, () => {
+
+      it(`should exist`, () => {
+        expect(sut.init).toBeDefined();
+      });
+
+      it(`should initialize with an svg element`, () => {
+        sut.init(elem);
+        expect(sut.elem).toBe(elem);
+      });
+
+    });
+
+    describe(`adding objects`, () => {
+
+      var circle;
+
+      beforeEach(() => {
+        sut.init(elem);
+        circle = {"circle" : {cx: 4, cy: 5, r: 3}};
+      });
+
+      it(`should be possible to add a new object`, () => {
+        var testObj = {test: {test: 4}};
+        expect(sut.addObject).toBeDefined();
+        sut.addObject(testObj);
+        expect(sut.objects.length).toBe(1);
+        expect(sut.objects[0]).toBe(testObj);
+      });
+
+      it(`should add the object to the html element`, () => {
+        sut.addObject(circle);
+        expect(sut.elem.html()).toBe(`<circle cx="${circle["circle"].cx}" cy="${circle["circle"].cy}" r="${circle["circle"].r}"></circle>`);
+      });
+
+      it(`should not add empty attributes`, () => {
+        var circle = {"circle" : {cx: 4, cy: 5, r: 3, boo: null, hoo: undefined}};
+        sut.addObject(circle);
+        expect(sut.elem.html()).toBe(`<circle cx="${circle["circle"].cx}" cy="${circle["circle"].cy}" r="${circle["circle"].r}"></circle>`);
+      });
+
+      describe(`circle`, () => {
+
+        it(`should exist`, () => {
+          expect(sut.circle).toBeDefined();
+        });
+
+        it(`should add a circle to the svg`, () => {
+          sut.circle(4, 5, 3);
+          expect(sut.elem.html()).toContain(`<circle cx="4" cy="5" r="3" stroke="black" stroke-width="1" fill="white"></circle>`);
+          sut.circle(4, 5, 3, "green", 1, "red");
+          expect(sut.elem.html()).toContain(`<circle cx="4" cy="5" r="3" stroke="green" stroke-width="1" fill="red"></circle>`);
+        });
+
+      });
+
+    });
+
+
+  });
+
 }
